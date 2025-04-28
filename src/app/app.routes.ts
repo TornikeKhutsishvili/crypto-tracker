1cc06d0969bd0cc090fae463bdd81c63dded1abc
Tornike Khutsishvili <tornikexucishvili0@gmail.com>
2025-04-28 19:26:40 +0400

    change router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,46 +1,66 @@
 import { authGuard } from './guards/auth.guard';
 import { Routes } from '@angular/router';
-import { CryptoListComponent } from './pages/crypto-list/crypto-list.component';
-import { CryptoDetailComponent } from './pages/crypto-detail/crypto-detail.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
-  { path: '',  component: CryptoListComponent},
-  { path: 'crypto/:id', component: CryptoDetailComponent},
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
+  { path: '',
+    loadComponent: () => import('./pages/crypto-list/crypto-list.component').then((m) => m.CryptoListComponent)
+  },
+
+  { path: 'crypto/:id',
+    loadComponent: () => import('./pages/crypto-detail/crypto-detail.component').then((m) => m.CryptoDetailComponent),
+    data: {
+      prerender: true,
+      getPrerenderParams: () => {
+        return { id: '1' };  // Return necessary parameters for prerendering
+      }
+    }
+  },
+
+  { path: 'register',
+    loadComponent: () => import('./pages/register/register.component').then((m) => m.RegisterComponent)
+  },
+
+  { path: 'login',
+    loadComponent: () => import('./pages/login/login.component').then((m) => m.LoginComponent)
+  },
+
   {
     path: 'profile',
-    loadComponent: () =>
-      import('./pages/profile/profile.component').then((m) => m.ProfileComponent),
+    loadComponent: () => import('./pages/profile/profile.component').then((m) => m.ProfileComponent),
     canActivate: [authGuard]
   },
+
   {
     path: 'reset-password',
     loadComponent: () => import('./pages/reset-password/reset-password.component').then(m => m.ResetPasswordComponent)
   },
+
   {
     path: 'edit-profile',
     loadComponent: () => import('./pages/edit-profile/edit-profile.component').then(m => m.EditProfileComponent),
     canActivate: [authGuard]
   },
+
   {
     path: 'market-cap',
     loadComponent: () => import('./pages/market-cap/market-cap.component').then(m => m.MarketCapComponent),
     canActivate: [authGuard]
   },
+
   { path: 'exchanges',
     loadComponent: () => import('./pages/exchanges/exchanges.component').then(m => m.ExchangesComponent),
     canActivate: [authGuard]
   },
+
   { path: 'most-visited',
     loadComponent: () => import('./pages/most-visited/most-visited.component').then(m => m.MostVisitedComponent),
     canActivate: [authGuard]
   },
+
   { path: 'favorites',
     loadComponent: () => import('./pages/favorites/favorites.component').then(m => m.FavoritesComponent),
     canActivate: [authGuard]
   },
+
   { path: '**', redirectTo: '' }, // should be last
 ];
